605ef823e9cda7ee4b035e8412806f0c502d644e
AAYamoldin <aayamoldin@gmail.com>
2019-09-09 19:23:02 +0300

    merge two hashs

--- lesson13_2.rb
+++ lesson13_2.rb
@@ -1,11 +1,16 @@
 def show_book book
+  puts "======================================="
   book.keys.each do |key|
     age = book[key]
     puts "Name is #{key} age is #{age}"
-end
+  end
+  puts "======================================="
+
 end
 
 book1 = {:mike => 54, :gus => 23, :lol => 22}
-show_book book1
 book2 = {:puk => 43, :struk => 11}
-show_book book2
+# show_book book2
+book1.merge! book2
+show_book book1
+
