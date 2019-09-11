47693592eb2690505dbd65950fdc977ee88b0363
AAYamoldin <aayamoldin@gmail.com>
2019-09-11 14:32:13 +0300

    статические функции

---
+++ lesson16_1.rb
@@ -0,0 +1,20 @@
+class Manager
+  def self.say_hi
+    puts "Hi"
+  end
+end
+
+class Hipster
+  def self.say_hi
+    puts "Hiiiiii, yo!"
+  end
+end
+
+class Jessie_Pinkman
+  def self.say_hi
+    puts "Hi, bitch!"
+  end
+end
+Manager.say_hi
+Hipster.say_hi
+Jessie_Pinkman.say_hi
