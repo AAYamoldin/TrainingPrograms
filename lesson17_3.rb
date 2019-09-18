97a8c7436bfbc4ae215535fad86cd424b2a5745c
AAYamoldin <aayamoldin@gmail.com>
2019-09-18 19:19:08 +0300

    Работа с файлами. открываем, закрываем, читаем построчно

--- lesson17_3.rb
+++ lesson17_3.rb
@@ -1,3 +1,13 @@
 output = File.open("./filenew.txt","a")
 output.write "something \n"
 output.close
+
+input = File.open("./test.txt", "r")
+totalearn = 0
+while (line = input.gets)
+   puts line
+  aa = line.split(",")
+  totalearn = totalearn + aa[1].to_i
+end
+input.close
+puts "total earn = #{totalearn}"
