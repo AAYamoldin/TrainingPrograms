9d93aede95d69992a3fbc518ecfbfe155328a716
Alexander Yamoldin <AAYamoldin@stud.kpfu.ru>
2020-04-24 17:31:33 +0300

    create a simple service with step-by-step cadeacademy hints

---
+++ codeacademy/Learn AngularJS 1.X/Lessons/Services/forecast.js
@@ -0,0 +1,9 @@
+app.factory('forecast', ['$http', function($http) {
+    return $http.get('https://s3.amazonaws.com/codecademy-content/courses/ltp4/forecast-api/forecast.json')
+        .success(function(data) {
+            return data;
+        })
+        .error(function(err) {
+            return err;
+        });
+}])
