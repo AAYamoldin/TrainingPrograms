b01c25877af632e0e1c5716ce181a334ef42893c
Alexander Yamoldin <AAYamoldin@stud.kpfu.ru>
2020-04-25 16:15:47 +0300

    fourth project. Work with services and directives. Start better understanding dependensies of variables

---
+++ codeacademy/Learn AngularJS 1.X/Projects/Top10/README.md
@@ -0,0 +1,3 @@
+##### Top 10 is creating an AngularJS app to chart popular TV shows. CSSs and almost all index.html was written by codeacademy. I did only part with AngularJS. https://aayamoldin.github.io/TrainingPrograms/codeacademy/Learn%20AngularJS%201.X/Projects/Top10
+
+![alt-text](result.png) 
---
+++ codeacademy/Learn AngularJS 1.X/Projects/Top10/js/services/shows.js
@@ -0,0 +1,9 @@
+app.factory('shows', ["$http", function ($http) {
+    return $http.get("https://s3.amazonaws.com/codecademy-content/courses/ltp4/shows-api/shows.json")
+        .success(function(data) {
+            return data;
+        })
+        .error(function(err){
+            return err;
+        })
+}])
