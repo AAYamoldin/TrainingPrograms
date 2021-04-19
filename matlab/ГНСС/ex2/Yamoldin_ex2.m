27c80031434ccf7c8fc7ffd9bea243de85a8c0d5
aayamoldin <aayamoldin@gmail.com>
2021-04-19 20:50:45 +0300

    Продолжили строить графики различных зависимостей для ГНСС систем из данных файла RINEX

--- matlab/ГНСС/ex2/Yamoldin_ex2.m
+++ matlab/ГНСС/ex2/Yamoldin_ex2.m
@@ -121,16 +121,16 @@ title('Графики фазовых измерений для 18 спутника GPS');
 xlabel('Время, с');
 ylabel('Фаза, рад');
 %% Графики фазовых измерений GALILEO
-L1C_GLON_3 = KZN2_RAW_GNSS_DATA.Galileo.E12.L7X;
-L1P_GLON_3 = KZN2_RAW_GNSS_DATA.Galileo.E12.L1X;
-L2C_GLON_3 = KZN2_RAW_GNSS_DATA.Galileo.E12.L8X;
-L2C_GLON_3 = KZN2_RAW_GNSS_DATA.Galileo.E12.L6X;
-L3X_GLON_3 = KZN2_RAW_GNSS_DATA.Galileo.E12.L5X;
-phases = [L1C_GLON_3', L1P_GLON_3',L2C_GLON_3',L2C_GLON_3', L3X_GLON_3' ];
+C1C = KZN2_RAW_GNSS_DATA.Galileo.E12.L7X;
+C1P = KZN2_RAW_GNSS_DATA.Galileo.E12.L1X;
+C2C = KZN2_RAW_GNSS_DATA.Galileo.E12.L8X;
+C2C = KZN2_RAW_GNSS_DATA.Galileo.E12.L6X;
+C3X = KZN2_RAW_GNSS_DATA.Galileo.E12.L5X;
+phases = [C1C', C1P',C2C',C2C', C3X' ];
 figure;
 hold all;
 for i = 1 :  size(phases,2)
-scatter(1: length(L1C_GLON_3),phases(:,i), 'LineWidth', 0.6);
+scatter(1: length(C1C),phases(:,i), 'LineWidth', 0.6);
 end
 legend('L7X','L1X','L8X','L6X','L5X');
 title('Графики фазовых измерений для 12 спутника GALILEO');
@@ -138,38 +138,234 @@ xlabel('Время, с');
 ylabel('Фаза, рад');
 
 %% Графики фазовых измерений BEIDOU-7
-L1C_GLON_3 = KZN2_RAW_GNSS_DATA.Beidou.C07.L1X;
-L1P_GLON_3 = KZN2_RAW_GNSS_DATA.Beidou.C07.L6I;
-L2C_GLON_3 = KZN2_RAW_GNSS_DATA.Beidou.C07.L7I;
-L2I_B_7 = KZN2_RAW_GNSS_DATA.Beidou.C07.L2I;
-L3X_GLON_3 = KZN2_RAW_GNSS_DATA.Beidou.C07.L5X;
-phases = [L1C_GLON_3', L1P_GLON_3',L2C_GLON_3',L2I_B_7', L3X_GLON_3' ];
+C1C = KZN2_RAW_GNSS_DATA.Beidou.C07.L1X;
+C1P = KZN2_RAW_GNSS_DATA.Beidou.C07.L6I;
+C2C = KZN2_RAW_GNSS_DATA.Beidou.C07.L7I;
+C2P = KZN2_RAW_GNSS_DATA.Beidou.C07.L2I;
+C3X = KZN2_RAW_GNSS_DATA.Beidou.C07.L5X;
+phases = [C1C', C1P',C2C',C2P', C3X' ];
 figure;
 hold all;
 for i = 1 :  size(phases,2)
-scatter(1: length(L1C_GLON_3),phases(:,i), 'LineWidth', 0.6);
+scatter(1: length(C1C),phases(:,i), 'LineWidth', 0.6);
 end
 legend('L7X','L1X','L8X','L6X','L5X');
-title('Графики фазовых измерений для 8 спутника BEIDOU');
+title('Графики фазовых измерений для 7 спутника BEIDOU');
 xlabel('Время, с');
 ylabel('Фаза, рад');
 
 %% Графики фазовых измерений GLONASS-3
-L1C_GLON_3 = KZN2_RAW_GNSS_DATA.GLONASS.R03.L1C;
-L1P_GLON_3 = KZN2_RAW_GNSS_DATA.GLONASS.R03.L1P;
-L2C_GLON_3 = KZN2_RAW_GNSS_DATA.GLONASS.R03.L2C;
+C1C = KZN2_RAW_GNSS_DATA.GLONASS.R03.L1C;
+C1P = KZN2_RAW_GNSS_DATA.GLONASS.R03.L1P;
+C2C = KZN2_RAW_GNSS_DATA.GLONASS.R03.L2C;
 L2P_GLON_3 = KZN2_RAW_GNSS_DATA.GLONASS.R03.L2P;
-L3X_GLON_3 = KZN2_RAW_GNSS_DATA.GLONASS.R03.L3X;
-phases = [L1C_GLON_3', L1P_GLON_3',L2C_GLON_3',L2P_GLON_3', L3X_GLON_3' ];
+C3X = KZN2_RAW_GNSS_DATA.GLONASS.R03.L3X;
+phases = [C1C', C1P',C2C',L2P_GLON_3', C3X' ];
 figure;
 hold all;
 for i = 1 :  size(phases,2)
-scatter(1: length(L1C_GLON_3),phases(:,i), 'LineWidth', 0.6);
+scatter(1: length(C1C),phases(:,i), 'LineWidth', 0.6);
 end
 legend('L1C','L1P','L2C','L2P','L3X');
 title('Графики фазовых измерений для 3 спутника GLONASS');
 xlabel('Время, с');
 ylabel('Фаза, рад');
+
+%% График псевдодальностных измерений для beidou - 7
+C1C = KZN2_RAW_GNSS_DATA.Beidou.C07.C1X;
+C1P = KZN2_RAW_GNSS_DATA.Beidou.C07.C5X;
+C2C = KZN2_RAW_GNSS_DATA.Beidou.C07.C2I;
+C2P = KZN2_RAW_GNSS_DATA.Beidou.C07.C7I;
+C3X = KZN2_RAW_GNSS_DATA.Beidou.C07.C6I;
+phases = [C1C', C1P',C2C',C2P', C3X' ];
+figure;
+hold all;
+for i = 1 :  size(phases,2)
+scatter(1: length(C1C),phases(:,i)/1000, 'LineWidth', 0.6);
+end
+legend('C1X','C5X','C2I','C7I','C6I');
+title('Графики фазовых измерений для 7 спутника BEIDOU');
+xlabel('Время, с');
+ylabel('Расстояние, км');
+%% График псевдодальностных измерений для GPS-18
+C1C = KZN2_RAW_GNSS_DATA.GPS.G05.C1C;
+C1P = KZN2_RAW_GNSS_DATA.GPS.G05.C1X;
+C2C = KZN2_RAW_GNSS_DATA.GPS.G05.C2W;
+C2P = KZN2_RAW_GNSS_DATA.GPS.G05.C2X;
+C3X = KZN2_RAW_GNSS_DATA.GPS.G05.C5X;
+phases = [C1C', C1P',C2C',C2P', C3X' ];
+figure;
+hold all;
+for i = 1 :  size(phases,2)
+scatter(1: length(C1C),phases(:,i)/1000, 'LineWidth', 0.6);
+end
+legend('C1C','C1X','C2W','C2X','C5X');
+title('Графики фазовых измерений для 18 спутника GPS');
+xlabel('Время, с');
+ylabel('Расстояние, км');
+
+%% График псевдодальностных измерений для Glonass - 3
+C1C = KZN2_RAW_GNSS_DATA.GLONASS.R03.C1C;
+C1P = KZN2_RAW_GNSS_DATA.GLONASS.R03.C1P;
+C2C = KZN2_RAW_GNSS_DATA.GLONASS.R03.C2C;
+C2P = KZN2_RAW_GNSS_DATA.GLONASS.R03.C2P;
+C3X= KZN2_RAW_GNSS_DATA.GLONASS.R03.C3X;
+phases = [C1C', C1P',C2C',C2P', C3X' ];
+figure;
+hold all;
+for i = 1 :  size(phases,2)
+scatter(1: length(C1C),phases(:,i)/1000, 'LineWidth', 0.6);
+end
+legend('C1C','C1P','C2C','C2P','C3X');
+title('Графики фазовых измерений для 3 спутника Glonass');
+xlabel('Время, с');
+ylabel('Расстояние, км');
+%% График псевдодальностных измерений для GALILEO-12
+C1X = KZN2_RAW_GNSS_DATA.Galileo.E12.C1X;
+C5X = KZN2_RAW_GNSS_DATA.Galileo.E12.C5X;
+C7X = KZN2_RAW_GNSS_DATA.Galileo.E12.C7X;
+C8X = KZN2_RAW_GNSS_DATA.Galileo.E12.C8X;
+C6X = KZN2_RAW_GNSS_DATA.Galileo.E12.C6X;
+phases = [C1X', C5X',C7X',C8X', C6X' ];
+figure;
+hold all;
+for i = 1 :  size(phases,2)
+scatter(1: length(C1X),phases(:,i)/1000, 'LineWidth', 0.6);
+end
+legend('C1X','C5X','C7X','C8X','C6X');
+title('Графики фазовых измерений для 12 спутника Galileo');
+xlabel('Время, с');
+ylabel('Расстояние, км');
+
+%% Графики изменения мощности сигнала 
+A = KZN2_RAW_GNSS_DATA.Beidou.C07.S1X;
+B = KZN2_RAW_GNSS_DATA.Beidou.C07.S5X;
+C = KZN2_RAW_GNSS_DATA.Beidou.C07.S6I;
+
+power = [A', B',C' ];
+figure;
+hold all;
+for i = 1 :  size(power,2)
+plot(power(:,i),'.');
+end
+legend('S1X','S5X','S6I');
+title('Графики изменения мощности сигнала для 7 спутника Beidou');
+xlabel('Время, с');
+ylabel('Мощность, Вт');
+
+A = KZN2_RAW_GNSS_DATA.GPS.G18.S1C;
+B = KZN2_RAW_GNSS_DATA.GPS.G18.S1X;
+C = KZN2_RAW_GNSS_DATA.GPS.G18.S2W;
+D = KZN2_RAW_GNSS_DATA.GPS.G18.S2X;
+E = KZN2_RAW_GNSS_DATA.GPS.G18.S5X;
+power = [A', B',C',D', E' ];
+figure;
+hold all;
+for i = 1 :  size(power,2)
+plot(power(:,i),'.');
+end
+legend('S1C','S1X','S2W','S2X','S5X');
+title('Графики изменения мощности сигнала для 18 спутника GPS');
+xlabel('Время, с');
+ylabel('Мощность, Вт');
+%% Графики изменения мощности сигнала 
+A = KZN2_RAW_GNSS_DATA.GLONASS.R03.S1C;
+B = KZN2_RAW_GNSS_DATA.GLONASS.R03.S1P;
+C = KZN2_RAW_GNSS_DATA.GLONASS.R03.S2C;
+D = KZN2_RAW_GNSS_DATA.GLONASS.R03.S2P;
+E = KZN2_RAW_GNSS_DATA.GLONASS.R03.S3X;
+power = [A', B',C',D', E' ];
+figure;
+hold all;
+for i = 1 :  size(power,2)
+plot(power(:,i),'.');
+end
+legend('S1C','S1P','S2C','S2P','S3X');
+title('Графики изменения мощности сигнала для 3 спутника Glonass');
+xlabel('Время, с');
+ylabel('Мощность, Вт');
+%% Графики изменения мощности сигнала 
+A = KZN2_RAW_GNSS_DATA.Galileo.E12.S1X;
+B = KZN2_RAW_GNSS_DATA.Galileo.E12.S5X;
+C = KZN2_RAW_GNSS_DATA.Galileo.E12.S7X;
+D = KZN2_RAW_GNSS_DATA.Galileo.E12.S8X;
+E = KZN2_RAW_GNSS_DATA.Galileo.E12.S6X;
+power = [A', B',C',D', E' ];
+figure;
+hold all;
+for i = 1 :  size(power,2)
+plot(power(:,i),'.');
+end
+legend('S1X','S5X','S7X','S8X','S6X');
+title('Графики изменения мощности сигнала для 12 спутника Galileo');
+xlabel('Время, с');
+ylabel('Мощность, Вт');
+
+
+
+%% ПЭС по фазе для GPS-18, Glonass - 3, Galileo - 12, Beidou - 7
+
+k = 40.308 * 10.^16;
+c = 3 * 10.^8;
+figure;
+hold on;
+
+% gps 
+f1 = 1575.42*10.^6;
+f2 = 1227.60*10.^6;
+f5 = 1176.45;
+
+lambda1 = c / f1;
+lambda2 = c / f2;
+
+L1 = KZN2_RAW_GNSS_DATA.GPS.G18.L1X;
+L2 = KZN2_RAW_GNSS_DATA.GPS.G18.L2X; 
+
+tec_l1_l2 = 1 / k * f1.^2*f2.^2 / (f1.^2 - f2.^2) * (L1 * lambda1 - L2 * lambda2);
+
+scatter(1: length(tec_l1_l2),tec_l1_l2, 'LineWidth', 0.6);
+
+% glonass -3
+
+f1 = 1602 + 3*9/16*10.^6;
+f2 = 1246 + 3*7/16*10.^6;
+
+lambda1 = c / f1;
+lambda2 = c / f2;
+
+L1 = KZN2_RAW_GNSS_DATA.GLONASS.R03.L1C;
+L2 = KZN2_RAW_GNSS_DATA.GLONASS.R03.L2C;
+tec_l1_l2 = 1 / k * f1.^2*f2.^2 / (f1.^2 - f2.^2) * (L1 * lambda1 - L2 * lambda2);
+
+scatter(1: length(tec_l1_l2),tec_l1_l2, 'LineWidth', 0.6);
+
+% galileo - 12
+f1 = 1575.42*10.^6;
+f2 = 1176.45;
+
+lambda1 = c / f1;
+lambda2 = c / f2;
+
+L1 = KZN2_RAW_GNSS_DATA.Galileo.E12.L1X;
+L2 = KZN2_RAW_GNSS_DATA.Galileo.E12.L5X;
+tec_l1_l2 = 1 / k * f1.^2*f2.^2 / (f1.^2 - f2.^2) * (L1 * lambda1 - L2 * lambda2);
+
+scatter(1: length(tec_l1_l2),tec_l1_l2, 'LineWidth', 0.6);
+
+
+%beiou - 7
+f1 = 1575.42*10.^6;
+f2 = 1191.79*10.^6;
+
+lambda1 = c / f1;
+lambda2 = c / f2;
+
+L1 = KZN2_RAW_GNSS_DATA.Beidou.C07.L1X;
+L2 = KZN2_RAW_GNSS_DATA.Beidou.C07.L5X;
+tec_l1_l2 = 1 / k * f1.^2*f2.^2 / (f1.^2 - f2.^2) * (L1 * lambda1 - L2 * lambda2);
+
+scatter(1: length(tec_l1_l2),tec_l1_l2, 'LineWidth', 0.6);
+
 %% Построение графиков данных суптников
 figure;
 plot([1:length(KZN2_RAW_GNSS_DATA.GPS.G01.C1C)]/60, KZN2_RAW_GNSS_DATA.GPS.G01.C1C/1000);
