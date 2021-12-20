adf0413e030131baa692136925ba6707dd0df434
AAYamoldin <AAYamoldin@stud.kpfu.ru>
2021-12-20 17:57:33 +0300

    Almost done day 4

---
+++ institute_c_programs/ITMO_Parallel_Algorithm/Task_11/Assignment11.cpp
@@ -0,0 +1,24 @@
+#include <iostream> 
+#include "mpi.h" 
+
+int main(int argc, char **argv) 
+{ 
+    int rank, size, prev, next; 
+    int buf[2]; 
+    MPI_Init(&argc, &argv); 
+    MPI_Status stats[2]; 
+    MPI_Comm_size(MPI_COMM_WORLD, &size); 
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank); 
+    prev = rank - 1; 
+    next = rank + 1; 
+    if (rank == 0) prev = size - 1; 
+    if (rank == size - 1) next = 0; 
+    MPI_Sendrecv(&rank, 1, MPI_INT, next, 5, &buf[0], 1, MPI_INT, prev, 5, MPI_COMM_WORLD, &stats[0]); 
+    MPI_Sendrecv(&rank, 1, MPI_INT, prev, 6, &buf[1], 1, MPI_INT, next, 6, MPI_COMM_WORLD, &stats[1]); 
+    //Your code here. 
+    //Here you need to display the number of the current process
+    //and what it receives from the previous and next processes. 
+	printf("Process %d from the previos %d from the next %d\n", rank, buf[0], buf[1]);
+
+    MPI_Finalize(); 
+    } 
---
+++ institute_c_programs/ITMO_Parallel_Algorithm/Task_9/Assignment9.cpp
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <mpi.h>
+#include <chrono>
+#include <iostream>
+
+
+int main(int argc,char *argv[]){
+    int size;
+
+    if (argc == 2)
+    {
+        size = atoi(argv[1]);
+    }
+    else
+    {
+        size = 1000000;
+    }
+    
+    int number[size];
+
+
+    MPI_Init(&argc, &argv); 
+    int world_rank; 
+    int world_size;
+    MPI_Comm_rank(MPI_COMM_WORLD,&world_rank);
+    MPI_Comm_size(MPI_COMM_WORLD,&world_size);
+    int gath_sum[world_size];
+    double start_time = MPI_Wtime();
+    int number1[size / world_size];   
+    //возможно здесь следует создать number если он не будет шериться      
+    if (world_rank == 0)
+    {   
+        for (int i = 0; i < size; i++)
+        {
+            number[i] = 1;
+        }
+    }
+
+    //All processes
+    MPI_Scatter(number, size / world_size, MPI_INT, &number1, size / world_size, MPI_INT, 0, MPI_COMM_WORLD);
+    // printf("I'm process %d , I received the array : ",world_rank);
+       
+    int sub_sum = 0;
+    for(int i=0 ; i<size / world_size ; i++){
+        // printf("%d ",number1[i]);
+        sub_sum = sub_sum + number1[i];
+    }
+    // printf(" My subsum is %d\n", sub_sum);
+    //printf("\n");    
+   
+    int sum = 0;
+        //для каскада закоммитить 
+    MPI_Reduce(&sub_sum, &sum, 1, MPI_INT, MPI_SUM,0,MPI_COMM_WORLD);
+        
+    if(world_rank == 0)
+    {
+        printf("The sum of array is: %d\n",sum);
+        printf("Execution time %f s.\n", MPI_Wtime() - start_time);
+    }
+
+    // MPI_Gather(&sub_sum, 1, MPI_INT, &gath_sum, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    // if (world_rank == 0)
+    // {  
+    //     for (int i = 0; i < world_size; i++)
+    //     { 
+    //         sum += gath_sum[i];
+    //     } 
+    //     printf("The sum of array is: %d\n", sum); 
+    //     double execution_time = MPI_Wtime() - start_time; 
+    //     printf("Execution time for cascade is: %f\n", execution_time);
+    // } 
+ 
+
+    MPI_Finalize();
+    return 0;
+ }
+
+ 
